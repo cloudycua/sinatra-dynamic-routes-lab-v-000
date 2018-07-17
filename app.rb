3092c1a29c9ff62c3e274d27b601c91eee1c7980
Christine Cua <cloudycua@gmail.com>
2018-07-17 21:42:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
   # accepts a name and renders the name backwards
   get '/reversename/:name' do
     @name = params[:name]
-    @name.reverse
+    "#{@name.reverse}""
   end
 
 end
