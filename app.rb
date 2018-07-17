56c2f189235d470a4b642fb7fa43e5689c3c2b3a
Christine Cua <cloudycua@gmail.com>
2018-07-17 21:48:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,5 +10,9 @@ class App < Sinatra::Base
   # accepts a number and retrns the square of that number
   # Note: values in params always come in as strings and
   # that the return value should also be a string
+  get '/square/:number' do
+    @square = params[:number].to_i * params[:number].to_i
+    @square.to_s
+  end
 
 end
